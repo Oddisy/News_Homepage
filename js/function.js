4527e6ba6488af8e39e557c7deead7591873d302
Oddisy <odunayoogunleye200@gmail.com>
2023-05-13 22:53:35 +0100

    Challenge Solution

--- js/function.js
+++ js/function.js
@@ -13,46 +13,7 @@
        itemBTwoChild = document.getElementById('itemBTwoChild');
 
 
-    /*  item-b ul first child functions  */
-      itemB.onclick = () =>{
-      // first item functions onclick
-       itemBChild.style.display ="block";
-        itemB.style.height = "43px";
-        // second item functions onclick of the first text
-          itemBOne.style.height = "25px";
-        itemBOneChild.style.display ="none";
-         // Third item functions onclick of the first text
-     itemBTwo.style.height = "0px";
-      itemBTwoChild.style.display ="none";
-     }
 
-    
-      itemBOne.onclick = () =>{
-          // first item functions onclick of the second text
-      itemB.style.height = "10px";
-      itemB.style.paddingBottom = "15px";
-       itemBChild.style.display ="none";
-         // second item functions onclick 
-       itemBOne.style.height = "35px";
-       itemBOneChild.style.display ="block";
-           // Third item functions onclick of the second text
-         itemBTwo.style.height = "0px";
-          itemBTwoChild.style.display ="none";
-      
-     }
-     
-     itemBTwo.onclick = () =>{
-      // first item functions onclick of the third text
-      itemBChild.style.display ="none";
-       itemB.style.paddingBottom = "15px";
-      itemB.style.height = "10px";
-      // second item functions onclick of the third text
-       itemBOneChild.style.display ="none";
-      itemBOne.style.height = "25px";
-      // Third item functions onclick of the second text
-        itemBTwoChild.style.display ="block";
-       itemBTwo.style.height = "38px";
-     }
 // @media queries
 
 menuLogo.onclick = () => {
